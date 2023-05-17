edb8d0d79b6afb85912019536aff01ba65c13e49
Andreas Leu on Erebor <andreas.leu@hotmail.com>
2023-05-17 13:40:32 +0200

    add UWP UDP script with a Python addon to send keystrokes via UDP

---
+++ Unity_UWP_UDP/Phython_send_Keyboard_UDP.py
@@ -0,0 +1,26 @@
+import socket
+import msvcrt
+import sys
+
+if len(sys.argv) < 2:
+	print('Usage: python keyboard.py IP')
+	sys.exit()
+
+UDP_IP = sys.argv[1]
+UDP_PORT = 48055
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+#sock.bind((UDP_IP, UDP_PORT))
+
+while True:
+    c = msvcrt.getch()
+    if c == b'\x00':
+    	continue
+    print('You entered: ' + str(c))
+    if c == b'q':
+        break
+    print('end: ' + str(c))
+    sock.sendto(c, (UDP_IP, UDP_PORT))
+
+sock.close()
